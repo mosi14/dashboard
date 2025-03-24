4fcbdedb06f63f32763e5b3508d76ee0135f7c25
mosio14 <mostafa.asadollahy@gmail.com>
2025-03-24 16:11:12 +0100

    add login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,44 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router";
 
 function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("https://dummyjson.com/auth/login", {
+        username,
+        password,
+      });
+      login(res.data); // Store token + user info
+      navigate("/profile");
+    } catch (err) {
+      alert("Login failed!");
+    }
+  };
+
   return (
-    <div>LoginPage</div>
-  )
+    <form onSubmit={handleLogin}>
+      <input
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        placeholder="Password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Login</button>
+    </form>
+  );
 }
 
-export default LoginPage
+export default LoginPage;
