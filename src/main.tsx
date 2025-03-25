fc5af83aa086737f395f35e79e97ecab57adfbb0
mosio14 <mostafa.asadollahy@gmail.com>
2025-03-25 10:48:05 +0100

    add toaster

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import { AuthProvider } from "./context/AuthContext";
+import { Toaster } from "react-hot-toast";
 import "./index.css";
 import App from "./App.tsx";
 
@@ -9,6 +10,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthProvider>
       <BrowserRouter>
+        <Toaster position="top-center" />
         <App />
       </BrowserRouter>
     </AuthProvider>
